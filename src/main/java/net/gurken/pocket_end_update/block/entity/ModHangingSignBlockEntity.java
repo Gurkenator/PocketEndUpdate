ee2c6661a96712ba8e5ee5ecff81e47e3431714a
Gurkenator <tobi_thelen@web.de>
2025-11-07 22:34:27 +0100

    -Fixed GUI for Eulium Hanging Sign

--- src/main/java/net/gurken/pocket_end_update/block/entity/ModHangingSignBlockEntity.java
+++ src/main/java/net/gurken/pocket_end_update/block/entity/ModHangingSignBlockEntity.java
@@ -2,12 +2,12 @@ package net.gurken.pocket_end_update.block.entity;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.block.entity.SignBlockEntity;
+import net.minecraft.world.level.block.entity.HangingSignBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
-public class ModHangingSignBlockEntity extends SignBlockEntity {
+public class ModHangingSignBlockEntity extends HangingSignBlockEntity {
     public ModHangingSignBlockEntity(BlockPos blockPos, BlockState blockState) {
-        super(ModBlockEntities.MOD_HANGING_SIGN.get(), blockPos, blockState);
+        super(blockPos, blockState);
     }
 
     @Override
